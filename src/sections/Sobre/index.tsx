9fad0829d3707d2dc3b0f1ee637eef92905cf9a9
Gabriel Oliveira <gabrieladscamp@gmail.com>
2025-12-29 14:58:23 -0300

    inicio da responsividade

--- src/sections/Sobre/index.tsx
+++ src/sections/Sobre/index.tsx
@@ -8,7 +8,7 @@ const Apresentacao = () => {
             <div className={styles.sobre}>
                 <div style={{display:"flex", alignItems:"center", fontSize:"64px", fontWeight:600, height:"140px"}}><p>Seja bem vindo</p> <img height={100} src={tag}/></div>
                 <div style={{fontSize:"32px", display:"flex", flexDirection:"column"}}>
-                    <p style={{margin:"0px", maxWidth:"800px"}}>Me chamo Gabriel, sou formado em Análise e Desenvolvimento de Sistemas Trabalho há mais de dois anos com desenvolvimento de software</p>
+                    <p style={{margin:"0px", maxWidth:"800px"}}>Me chamo Gabriel, sou formado em Análise e Desenvolvimento de Sistemas e trabalho há mais de dois anos com desenvolvimento de software</p>
                     {/* <p style={{marginTop:"4px"}}>Sou desenvolvedor full-stack e trabalho há mais de dois anos com desenvolvimento de software</p> */}
                 </div>
             </div>
