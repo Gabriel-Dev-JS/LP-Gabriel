c2986f0c5a67aa6f9cd22c132d5b98f48c84d5a8
Gabriel-Dev-JS <GabrielAdsCamp@gmail.com>
2025-12-31 01:14:18 -0300

    criacao do slide e ajustes na responsividade

--- src/sections/Projetos/index.tsx
+++ src/sections/Projetos/index.tsx
@@ -1,10 +1,18 @@
 import type { ReactNode } from "react";
-import Card from "../../componentes/Card"
-import styles from "./style.module.css"
-import { SiExpress, SiFlask, SiJest, SiPrisma, SiSqlite } from "react-icons/si";
-import { IoLogoHtml5, IoLogoJavascript } from "react-icons/io5";
-import { FaCss3Alt, FaDocker, FaNode } from "react-icons/fa";
+import { useEffect, useState } from "react";
 import { BiLogoPostgresql, BiLogoTypescript } from "react-icons/bi";
+import { FaCss3Alt, FaDocker, FaNode } from "react-icons/fa";
+import { IoLogoHtml5, IoLogoJavascript } from "react-icons/io5";
+import { SiExpress, SiFlask, SiJest, SiPrisma, SiSqlite } from "react-icons/si";
+
+import Card from "../../componentes/Card";
+import styles from "./style.module.css";
+
+import 'swiper/css';
+// import 'swiper/css/navigation';
+import "swiper/css/pagination";
+import { Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 
 interface Projeto {
@@ -15,45 +23,103 @@ interface Projeto {
 
 const Projetos = () => {
 
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+    useEffect(() => {
+    const handleResize = () => {
+        setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+
     const projetos:Projeto[] = [
         {
             titulo: "Gym Planner",
             conteudo: "É um gerenciador de treinos de academia, o professor cria os exercicios e o aluno tem acesso, no login possui uma validação para identificar se o usuario é professor ou aluno.",
             // conteudo: "Foi um projeto feito na faculdade, na materia de desenvolvimento de aplicaçãoes rapidas em flask, este trabalho foi feito em 3 pessoas, onde eu atuei no backend, outro colega no front-end e o terceito sendo PO levantando requisitos e criando os cards no trello.",
-            tecnologias: [<SiFlask />, <SiSqlite />, <IoLogoHtml5 />, <FaCss3Alt />, <IoLogoJavascript />]
+            tecnologias: [<SiFlask className={styles.iconesTecnologias}/>, <SiSqlite className={styles.iconesTecnologias}/>, <IoLogoHtml5 className={styles.iconesTecnologias}/>, <FaCss3Alt className={styles.iconesTecnologias}/>, <IoLogoJavascript className={styles.iconesTecnologias}/>]
             // tecnologias: ["Flask", "SQLite", "HTML", "CSS", "JavaScript"]
         },
         {
             titulo: "Backend ToDo",
             conteudo: "Backend de um ToDo que eu criei para organizar o meu dia, projeto caseiro e de rapido desenvolvimento criado para o intuito auxiliar nas minhas tarefas diarias",
-            tecnologias: [<SiExpress />, <FaNode />, <BiLogoTypescript />, <SiSqlite />]
+            tecnologias: [<SiExpress className={styles.iconesTecnologias}/>, <FaNode className={styles.iconesTecnologias}/>, <BiLogoTypescript className={styles.iconesTecnologias}/>, <SiSqlite className={styles.iconesTecnologias}/>]
             // tecnologias: ["express.js", "Node.js", "Typescript", "SQLite"]
         },
         {
             titulo: "Organizador Financeiro",
             conteudo: "Criei uma carteira onde o usuario consegue gerir o seu salario, inputando os valores arrecadados e gastos durante o mês, ajudando nassim no controle dos gastos mensais",
-            tecnologias: [<SiExpress />, <FaNode />, <BiLogoTypescript />, <BiLogoPostgresql />, <SiPrisma />, <FaDocker />, <SiJest />]
+            tecnologias: [<SiExpress className={styles.iconesTecnologias}/>, <FaNode className={styles.iconesTecnologias}/>, <BiLogoTypescript className={styles.iconesTecnologias}/>, <BiLogoPostgresql className={styles.iconesTecnologias}/>, <SiPrisma className={styles.iconesTecnologias}/>, <FaDocker className={styles.iconesTecnologias}/>, <SiJest className={styles.iconesTecnologias}/>]
             // tecnologias: ["express.js", "Node.js", "Typescript", "Postgres", "Prisma", "docker", "jest"]
         }
     ]
 
     return(
         <div className={styles.containerProjeto} id="projetos">
-            {projetos.map((projeto) => (
+            
+            {isMobile ? (
+            <Swiper
+                modules={[Navigation, Pagination]}
+                navigation
+                pagination={{ clickable: true }}
+                loop={true}
+                slidesPerView={1}
+            >
+                {projetos.map((projeto) => (
+                // <SwiperSlide key={projeto.titulo}>
+                <SwiperSlide className={styles.meuSwiper} key={projeto.titulo}>
+                    <Card
+                    titulo={projeto.titulo}
+                    conteudo={projeto.conteudo}
+                    tecnologia={
+                        <ul className={styles.listaTecnologias}>
+                        {projeto.tecnologias.map((tech, index) => (
+                            <li key={index}>{tech}</li>
+                        ))}
+                        </ul>
+                    }
+                    />
+                </SwiperSlide>
+                ))}
+            </Swiper>
+            ) : (
+            <div className={styles.containerProjeto}>
+                {projetos.map((projeto) => (
                 <Card
                     key={projeto.titulo}
                     titulo={projeto.titulo}
                     conteudo={projeto.conteudo}
                     tecnologia={
                     <ul className={styles.listaTecnologias}>
-                        {projeto.tecnologias.map((tech) => (
-                        <li style={{width:"1000%"}} key={tech}>{tech}</li>
+                        {projeto.tecnologias.map((tech, index) => (
+                        <li key={index}>{tech}</li>
+                        ))}
+                    </ul>
+                    }
+                />
+                ))}
+            </div>
+            )}
+
+            
+            {/* {projetos.map((projeto) => (
+                <Card
+                    key={projeto.titulo}
+                    titulo={projeto.titulo}
+                    conteudo={projeto.conteudo}
+                    tecnologia={
+                    <ul className={styles.listaTecnologias}>
+                        {projeto.tecnologias.map((tech, index) => (
+                        <li key={index}>{tech}</li>
                         ))}
                     </ul>
                     }
                 >
                 </Card>
-            ))}
+            ))} */}
             {/* <Card titulo={"teste 2"} conteudo={<p>teste</p>}/>
             <Card titulo={"teste 3"} conteudo={<p>teste</p>}/> */}
         </div>
